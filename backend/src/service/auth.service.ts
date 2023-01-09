ed6c17610b0a04b584d85ac73a18edeae741027d
Ector Cunha <ectorvccunha@outlook.com>
2023-01-09 17:47:44 -0300

    finalizando o desafio

--- backend/src/service/auth.service.ts
+++ backend/src/service/auth.service.ts
@@ -6,7 +6,10 @@ dotenv.config()
 
 const token = process.env.SECRET_JWT
 
+// Comparação com o banco de dados
 export const loginService = async (username: string) => Login.findOne({username: username}).select('+password')
 
+
+// Geração do token
 export const generateToken = (id: any) => jwt.sign({id: id}, token, {expiresIn: 10})
 export const refreshToken = (id: any) => jwt.sign(generateToken, 'secret', {expiresIn: 30})
